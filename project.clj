02c4019c79e94965a40bba8cfd9426e9deecf11f
Alan Busby <abusby@gracenote.com>
2013-09-19 17:31:52 +0900

    Updated version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-aws-cf "0.1.0-SNAPSHOT"
-  :description "Clojure Amazon Cloud Formation library."
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clj-aws-cf "1.0.0-SNAPSHOT"
+  :description "Clojure wrapper for Amazon's AWS CloudFormation API."
+  :url "http://github.com/thebusby/clj-aws-cf"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
                  [com.amazonaws/aws-java-sdk "1.5.5"]])
